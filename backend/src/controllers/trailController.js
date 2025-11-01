243c9fec3c75ba931c4a1565b3e7a57c632a271d
Ricardo <ricardo.jnr25@gmail.com>
2025-11-01 15:38:19 -0300

    Alteração de palavras chaves do português para o Inglês

--- backend/src/controllers/trailController.js
+++ backend/src/controllers/trailController.js
@@ -1,11 +1,11 @@
 // Simulação de dados
-const trilhas = [
-  { id: 1, nome: "Pedra da Rajada", local: "Maranguape - CE", dificuldade: "Moderado", distanciaKm: 8.0 },
-  { id: 2, nome: "Trilha do Parque do Cocó", local: "Fortaleza - CE",  dificuldade: "Fácil", distanciaKm: 3.0 },
-  { id: 3, nome: "Trilha da Esperança", local: "Guaramiranga - CE" ,dificuldade: "Difícil", distanciaKm: 15.0 }
+const trails = [
+  { id: 1, name: "Pedra da Rajada", location: "Maranguape - CE", difficulty: "Moderado", distance: 8.0 },
+  { id: 2, name: "Trilha do Parque do Cocó", location: "Fortaleza - CE",  difficulty: "Fácil", distance: 3.0 },
+  { id: 3, name: "Trilha da Esperança", location: "Guaramiranga - CE" ,difficulty: "Difícil", distance: 15.0 }
 ];
 
-// Função para listar todas as trilhas
-exports.listarTrilhas = (req, res) => {
-  res.json(trilhas);
+// Função para listar todas as trails
+exports.listartrails = (req, res) => {
+  res.json(trails);
 };
