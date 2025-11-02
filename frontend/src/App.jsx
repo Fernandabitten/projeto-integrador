3b380343e00f13203f639afcacc55c5ec1730fe4
Fernandabitten <fernandab.bittencourt@gmail.com>
2025-11-01 21:46:01 -0300

    feat(front): implementa componente card das trilhas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,3 @@
-// import { useState } from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Sidebar from './components/Sidebar';
-// import Home from './pages/Home';
-// import MyTrails from './pages/MyTrails';
-
-// function App() {
-//   const [currentPage, setCurrentPage] = useState('home');
-//   return (
-//     <div className="flex min-h-screen bg-gray-50 font-sans">
-//       {/* Passa o estado e a função para o Sidebar */}
-//       <Sidebar currentPage={currentPage} setCurrentPage={setCurrentPage} />
-//       {console.log(currentPage)}
-//       <main className="flex-1 flex flex-col md:ml-0 ml-0 md:pl-0 pt-16 md:pt-0">
-//         {/* Renderiza a página conforme o estado */}
-//         {/* {currentPage === 'home' && <Home />}
-//         {currentPage === 'minhas-trilhas' && <MyTrails />} */}
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/minhas-trilhas" element={<MyTrails />} />
-//           </Routes>
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
@@ -33,9 +5,9 @@ import MyTrails from './pages/MyTrails';
 
 function App() {
   return (
-    <div className="flex min-h-screen bg-gray-50 font-sans text-text">
+    <div className="flex h-screen overflow-hidden bg-gray-50 font-sans text-text">
       <Sidebar />
-      <main className="flex-1 flex flex-col p-6">
+      <main className="flex-1 overflow-y-auto bg-gray-50 p-4">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/minhas-trilhas" element={<MyTrails />} />
