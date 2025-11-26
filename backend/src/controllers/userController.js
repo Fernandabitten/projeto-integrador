5e2970ee9ec577a5c1d5198734a22d85f1af2626
Ricardo <ricardo.jnr25@gmail.com>
2025-11-25 21:50:46 -0300

    Atualização de código

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -4,9 +4,6 @@ import { loginUserCore } from "../core/loginUserCore.js";
 import { generateToken } from "../utils/auth.js";
 
 
-// "Banco de dados" em memória
-const users = [];
-
 // ===========================
 //   POST /auth/register
 // ===========================
@@ -40,39 +37,3 @@ export async function loginUser(req, res) {
   }
 }
 
-// ===========================
-//      PUT /auth/update
-// ===========================
-export async function updateUser(req, res) {
-  try {
-    const { id, name, email } = req.body;
-
-    // Verificação básica
-    if (!id) {
-      return sendError(res, 400, "ID do usuário é obrigatório.");
-    }
-
-    // Buscar usuário
-    const user = users.find((u) => u.id === id);
-
-    if (!user) {
-      return sendError(res, 404, "Usuário não encontrado.");
-    }
-
-    // Atualizar apenas campos enviados
-    if (name) user.name = name;
-    if (email) user.email = email;
-
-    if (data.password) {
-  user.password = await bcrypt.hash(data.password, 10);
-    }
-
-    return sendSuccess(res, 200, {
-      message: "Usuário atualizado com sucesso!",
-      user,
-    });
-
-  } catch (err) {
-    return sendError(res, 500, "Erro ao atualizar usuário.");
-  }
-}
